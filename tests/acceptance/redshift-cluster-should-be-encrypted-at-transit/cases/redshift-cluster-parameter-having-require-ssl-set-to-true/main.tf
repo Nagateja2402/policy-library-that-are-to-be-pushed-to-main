1e7a515247c78dd8b6c04bbd13e8eb89bd42a59c
Nagateja2402 <Nagateja590@gmail,com>
2024-08-06 13:29:56 +0530

    make fmt

--- tests/acceptance/redshift-cluster-should-be-encrypted-at-transit/cases/redshift-cluster-parameter-having-require-ssl-set-to-true/main.tf
+++ tests/acceptance/redshift-cluster-should-be-encrypted-at-transit/cases/redshift-cluster-parameter-having-require-ssl-set-to-true/main.tf
@@ -12,13 +12,13 @@ provider "aws" {
 }
 
 resource "aws_redshift_cluster" "rscluster" {
-  cluster_identifier = "tf-redshift-cluster"
-  database_name      = "newdb"
-  master_username    = "exampleuser"
-  master_password    = "Newpass12345678"
-  node_type          = "dc1.large"
-  cluster_type       = "single-node"
-  publicly_accessible = true
+  cluster_identifier           = "tf-redshift-cluster"
+  database_name                = "newdb"
+  master_username              = "exampleuser"
+  master_password              = "Newpass12345678"
+  node_type                    = "dc1.large"
+  cluster_type                 = "single-node"
+  publicly_accessible          = true
   cluster_parameter_group_name = aws_redshift_parameter_group.new_parameter_grp.name
 }
 
--- tests/acceptance/redshift-cluster-should-be-encrypted-at-transit/cases/redshift-cluster-with-no-parameter-group/main.tf
+++ tests/acceptance/redshift-cluster-should-be-encrypted-at-transit/cases/redshift-cluster-with-no-parameter-group/main.tf
@@ -12,11 +12,11 @@ provider "aws" {
 }
 
 resource "aws_redshift_cluster" "rscluster" {
-  cluster_identifier = "tf-redshift-cluster"
-  database_name      = "newdb"
-  master_username    = "exampleuser"
-  master_password    = "Newpass12345678"
-  node_type          = "dc1.large"
-  cluster_type       = "single-node"
+  cluster_identifier  = "tf-redshift-cluster"
+  database_name       = "newdb"
+  master_username     = "exampleuser"
+  master_password     = "Newpass12345678"
+  node_type           = "dc1.large"
+  cluster_type        = "single-node"
   publicly_accessible = true
 }
